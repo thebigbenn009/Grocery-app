2940caaa8d28644627bc64d928d01ee3c8317647
Benedcit <aliubenedict@gmail.com>
2023-11-05 16:08:46 +0100

    added remove item

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
 import Form from "./components/Form";
+import Items from "./components/Items";
 
 const App = () => {
   const [items, setItems] = useState([]);
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: Date.now(),
+    };
+    setItems([...items, newItem]);
+  };
+  const removeItem = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+  };
   return (
     <section className="section-center">
-      <Form />
+      <Form addItem={addItem} />
+      <Items items={items} removeItem={removeItem} />
     </section>
   );
 };
---
+++ src/components/SingleItem.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(item.completed);
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{
+          textDecoration: isChecked && "line-through",
+          textTransform: "capitalize",
+        }}
+      >
+        {item.name}
+      </p>
+      <button
+        type="button"
+        className="btn remove-btn"
+        onClick={() => removeItem(item.id)}
+      >
+        delete
+      </button>
+    </div>
+  );
+};
+
+export default SingleItem;
